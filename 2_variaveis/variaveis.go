40fe5f73d3c475a05d2a451100a6d6449a832086
renato@likwi.com.br <renato@likwi.com.br>
2023-06-13 20:55:10 -0300

    declaracões de variáveis

---
+++ 2_variaveis/variaveis.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "teste1"
+	fmt.Println(variavel1)
+
+	variavel2 := "teste2" //declaracao por inferencia
+	fmt.Println(variavel2)
+
+}

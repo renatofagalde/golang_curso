b53da76221ed8a5ec23898d12f8472dd7ac7d763
renato@likwi.com.br <renato@likwi.com.br>
2023-06-13 20:43:44 -0300

    importando via modulo

---
+++ pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever uma mensagem no console
+func EscreverTesteMensagem() {
+	fmt.Println("escrevendo do arquivo main")
+}
---
+++ pacotes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	auxiliar.EscreverTesteMensagem()
+	fmt.Println("escrevendo do arquivo main do modulo")
+}

12de18df46c110c33bf8af5e9f65d4fc8e573f5e
renato@likwi.com.br <renato@likwi.com.br>
2023-06-13 20:57:02 -0300

    declaracões de variáveis por inferencias

--- 2_variaveis/variaveis.go
+++ 2_variaveis/variaveis.go
@@ -9,4 +9,6 @@ func main() {
 	variavel2 := "teste2" //declaracao por inferencia
 	fmt.Println(variavel2)
 
+	variavel3, variavel4 := "valor por inferência3", " por inferencia 4"
+	fmt.Println(variavel3, variavel4)
 }

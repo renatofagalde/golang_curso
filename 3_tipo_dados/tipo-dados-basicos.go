221ec58fc58bbe4cf7d9377014ce5c0429324c59
renato@likwi.com.br <renato@likwi.com.br>
2023-06-14 09:44:03 -0300

    tipos int

---
+++ 3_tipo_dados/tipo-dados-basicos.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero int8 = 100
+	fmt.Println(numero)
+
+	//alias INT32 = RUNE
+	var numero2 rune = 12345
+	fmt.Println(numero2)
+
+	//byte = int8
+	var numero3 byte = 123
+	fmt.Println(numero3)
+}

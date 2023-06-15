7a73b5e0d7c406f0982599d6cb737df8a1281346
renato@likwi.com.br <renato@likwi.com.br>
2023-06-15 08:37:09 -0300

    comentário do motivo de usar um underline

--- 4_funcoes/funcoes.go
+++ 4_funcoes/funcoes.go
@@ -24,6 +24,9 @@ func main() {
 	soma01, subtracao01 := calculo(10, 20)
 	fmt.Println("Valores retornados em multiplas funcoes soma: %d, subtracao01: %d", soma01, subtracao01)
 
+	//o underline significa que é para ignorar um dos retornos,
+	//lembrando que isto é obrigatório pq go não compila com uma variável
+	//que não está sendo usada
 	_, subtracao02 := calculo(10, 20)
 	fmt.Println("Valor da subtracao ignorando a soma ", subtracao02)
 }

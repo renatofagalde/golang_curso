cd369615a6fc07adf11c2eb28790b90b40fee0ee
renato@likwi.com.br <renato@likwi.com.br>
2023-06-16 14:40:25 -0300

    map

--- 10_maps/map.go
+++ 10_maps/map.go
@@ -13,4 +13,22 @@ func main() {
 	}
 
 	fmt.Println(usuario)
+
+	//map com map
+	usuario02 := map[string]map[string]string{
+		"pessoa01": {"nome01": "zé ruela", "sobrenome02": "turma da monica"},
+		"curso":    {"nome": "culinária", "escola": "diadema cursos online"},
+	}
+
+	fmt.Println(usuario02)
+
+	//deletando chave
+	delete(usuario02, "curso")
+	fmt.Println(usuario02)
+
+	usuario02["signo"] = map[string]string{
+		"nome": "virgem",
+	}
+
+	fmt.Println(usuario02)
 }

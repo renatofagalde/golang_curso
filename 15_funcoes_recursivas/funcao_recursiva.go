b95da4a7d44add4332e96d56c41a7f1c48bf6bb4
renato@likwi.com.br <renato@likwi.com.br>
2023-06-17 11:33:17 -0300

    chamando funcao anonima

---
+++ 15_funcoes_recursivas/funcao_recursiva.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fibonacci(posicao uint) uint {
+	// TODO
+	// usar o memoization
+
+	if posicao <= 1 {
+		return posicao
+	}
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+}
+func main() {
+
+	posicao := uint(100)
+	fmt.Println(fibonacci(posicao))
+}

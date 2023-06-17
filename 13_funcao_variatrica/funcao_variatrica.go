2edecd1de2d70b8d4ad28ec5ff218cae567dda14
renato@likwi.com.br <renato@likwi.com.br>
2023-06-17 11:18:11 -0300

    comentários

--- 13_funcao_variatrica/funcao_variatrica.go
+++ 13_funcao_variatrica/funcao_variatrica.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// apenas um parametro variático por funcao e ele deve ser sempre o ultimo
 func soma(numeros ...int) int {
 	total := 0
 	for _, numero := range numeros {
@@ -13,4 +14,9 @@ func soma(numeros ...int) int {
 func main() {
 	totalizador01 := soma(1, 2, 3, 4, 5, 1, 2, 4, 5, 6, 1)
 	fmt.Println(totalizador01)
+
+	fmt.Println("----")
+	totalizador01 = soma()
+	fmt.Println(totalizador01)
+
 }

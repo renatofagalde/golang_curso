59b69c16176c6d9a52f25989ab8a8e97b174793c
renato@likwi.com.br <renato@likwi.com.br>
2023-06-17 14:48:02 -0300

    trabalhando com métodos

--- 21_metodos/metodos.go
+++ 21_metodos/metodos.go
@@ -8,11 +8,26 @@ type usuario struct {
 }
 
 func (u usuario) salvar() {
-	fmt.Printf("salvando usuário %s no banco de dados", u.nome)
+	fmt.Printf("salvando usuário %s no banco de dados\n", u.nome)
+}
+
+func (u usuario) maiorDeIdade() bool {
+	return u.idade >= 18
+}
+
+// por conta do * isto é uma referencia
+// por isto ele troca
+func (u *usuario) fazerAniversario() {
+	u.idade++
 }
 
 func main() {
-	usuario := usuario{"john doe", 10}
+	usuario := usuario{"john doe", 20}
 	usuario.salvar()
 
+	fmt.Printf("usuario %s é maior de idade? %v\n", usuario.nome, usuario.maiorDeIdade())
+
+	usuario.fazerAniversario()
+
+	fmt.Println(usuario)
 }

b1d3308df1e961f1affe88c948dcfe85a00db74e
renato@likwi.com.br <renato@likwi.com.br>
2023-06-17 14:15:30 -0300

    funcoes com ponteiros

---
+++ 19_funcoes_e_ponteiros/funcoes_e_ponteiros.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func inverterSinal(numero int) int {
+	return numero * -1
+}
+
+func inverterSinalComPonteiro(numero *int) int {
+
+	//fmt.Println("numero: ", &numero, " memória: ", numero)
+
+	return *numero * -1
+}
+
+func trocarNome(nome *string) {
+	fmt.Println("variavel nome: ", *nome, " com endereco de mémoria: ", &nome)
+	//valor := *nome
+	*nome = *nome + "_teste"
+	fmt.Println("variavel nome após troca via endereco de memória: ", *nome, " com endereco de mémoria: ", &nome)
+	fmt.Println(*nome)
+}
+
+func main() {
+	//numero := 20
+	//numeroInvert_ido := inverterSinal(numero) //passando uma copia do valor
+	//fmt.Println(numeroInvertido)
+
+	numeroNovo := 40
+	numeroInvertido01 := inverterSinalComPonteiro(&numeroNovo) //& informando o endereco de mémoria
+	fmt.Println("numeroNovo:", numeroNovo, " numeroIvertido: ", numeroInvertido01)
+
+	nome := "renato"
+	trocarNome(&nome)
+	fmt.Println("nome apos a chamada do ponteiro:", nome)
+}

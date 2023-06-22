9f6a1540673e8aaf58b26897210003a290e237b1
renato@likwi.com.br <renato@likwi.com.br>
2023-06-22 13:48:00 -0300

    buscando servidores

--- 24_aplicacao_linha_comando/app/app.go
+++ 24_aplicacao_linha_comando/app/app.go
@@ -13,16 +13,22 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca ips e nomes de servidor na internet"
 
-	app.Commands = []cli.Command{{
-		Name:  "ip",
-		Usage: "Usage ips de endereços",
-		Flags: []cli.Flag{
-			cli.StringFlag{
-				Name:  "host",
-				Value: "devbook.com.br",
-			},
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
 		},
+	}
+	app.Commands = []cli.Command{{
+		Name:   "ip",
+		Usage:  "Usage ips de endereços",
+		Flags:  flags,
 		Action: buscarIps,
+	}, {
+		Name:   "servidores",
+		Usage:  "busca servidores na internet",
+		Flags:  flags,
+		Action: buscarServidores,
 	}}
 
 	return app
@@ -39,5 +45,16 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+	servidores, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	for _, servidor := range servidores {
+		fmt.Println(servidor)
 	}
 }

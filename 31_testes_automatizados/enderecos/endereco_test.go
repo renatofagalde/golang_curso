779024a9c2093c43b5194f4ed99d1c8413ea526e
renato@likwi.com.br <renato@likwi.com.br>
2023-06-24 21:12:31 -0300

    gerando relatório de cobertura de testes

--- 31_testes_automatizados/enderecos/endereco_test.go
+++ 31_testes_automatizados/enderecos/endereco_test.go
@@ -8,13 +8,35 @@ type cenarioTest struct {
 	enderecoEsperado string
 }
 
+const naoLocalizado = "Não localizado"
+
 func TestTipoDeEndereco(t *testing.T) {
+	t.Parallel()
 
 	cenariosTest := []cenarioTest{
 		{"Rua Abc", "Rua"},
 		{"Avenida apulista", "Avenida"},
 		{"Rodovia Abc", "Rodovia"},
-		{"Praça das rodas", "Não localizado"},
+		{"Praça das rodas", naoLocalizado},
+	}
+
+	for _, cenario := range cenariosTest {
+		tipoNoTeste := TipoDeEndereco(cenario.enderecoTeste)
+		if tipoNoTeste != cenario.enderecoEsperado {
+			t.Errorf("O tipo recebido é diferente do esperado! Esperava \"%s\" e recebeu \"%s\"",
+				cenario.enderecoEsperado, tipoNoTeste)
+		}
+	}
+}
+
+func TestTipoDeEnderecoErrado(t *testing.T) {
+	t.Parallel()
+
+	cenariosTest := []cenarioTest{
+		{"Ruas Abc", "Não localizado"},
+		{"Avenida apulista", "Avenida"},
+		{"Rodovia Abc", "Rodovia"},
+		{"Praça das rodas", naoLocalizado},
 	}
 
 	for _, cenario := range cenariosTest {

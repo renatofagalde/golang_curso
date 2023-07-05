8c1f41cd49c9acfa596cd77128cfbbc87b0d2654
renato@likwi.com.br <renato@likwi.com.br>
2023-07-05 08:38:52 -0300

    extraindo funcao

--- 34_http/main.go
+++ 34_http/main.go
@@ -8,5 +8,13 @@ import (
 func main() {
 	//URI - Identifcador do recurso
 
+	http.HandleFunc("/home", home())
+
 	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func home() func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Olá mundo!"))
+	}
 }

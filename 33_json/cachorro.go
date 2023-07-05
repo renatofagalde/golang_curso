78735d5890578e1397ab69375dbaa8987d1ec293
renato@likwi.com.br <renato@likwi.com.br>
2023-07-04 22:35:08 -0300

    marshal e unmarshal

--- 33_json/cachorro.go
+++ 33_json/cachorro.go
@@ -2,7 +2,7 @@ package cachorro
 
 // tipo de dado
 type Cachorro struct {
-	Nome  string `json:"nome"`
+	Nome  string `json:"nome"` // `json:"-"`  para ignorar no json
 	Raca  string `json:"raca"`
 	Idade uint   `json:"idade"`
 }
